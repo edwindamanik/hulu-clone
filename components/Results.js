a33c8f1df1357dcf4d2e4825926d9745be62e10e
Edwin Immanuel Damanik <58164595+edwindamanik@users.noreply.github.com>
2021-12-24 22:35:53 +0700

    Update Results.js

--- components/Results.js
+++ components/Results.js
@@ -5,11 +5,11 @@ function Results({ results }) {
 
     console.log(results);
     return (
-        <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
-            {results.map((result) => (
+        <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
+            {results.map(result => (
                 <Thumbnail key={result.id} result={result} />
             ))}
-        </FlipMove>
+        </div>
     )
 }
 
